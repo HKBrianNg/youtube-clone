d641b15fe35a8104c4344c1a93b4f671ef271fe8
Brian Ng <ngshunchiang@hotmail.com>
2022-07-16 09:31:19 +0800

    fix remove app.css issue

--- src/page/home/HomePage.js
+++ src/page/home/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../component/navbar/Navbar';
 import HomeContent from '../../component/homecontent/HomeContent';
 import Sidebar from '../../component/sidebar/Sidebar';
+import './HomePage.css';
 
 function HomePage() {
     return (
--- src/page/search/SearchPage.js
+++ src/page/search/SearchPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../../component/navbar/Navbar';
 import Sidebar from '../../component/sidebar/Sidebar';
 import SearchContent from '../../component/searchcontent/SearchContent';
-
+import './SearchPage.css';
 
 function SearchPage() {
     return (
